44bbb0ff3a361c923c4220cfa3e75173769c3043
maksim.agarev <maksim@agarev>
2022-07-08 15:48:04 +0300

    initial #2

--- owSimpleLazyload.js
+++ owSimpleLazyload.js
@@ -1,39 +1,42 @@
-class owSimpleLazyload {
-  constructor(callback, observer = {}) {
-    this.IO = new IntersectionObserver(this.common.bind(undefined, callback), observer);
-  }
+(function(w, d){
+  'use strict';
+  w.owSimpleLazyload = class {
+    constructor(callback, observer = {}) {
+      this.IO = new IntersectionObserver(this.common.bind(undefined, callback), observer);
+    }
 
-  init() {
-    const images = document.querySelectorAll('[data-lazyload]');
+    init() {
+      const images = d.querySelectorAll('[data-lazyload]');
 
-    if (images) {
-      for (const image of images) {
-        this.IO.observe(image);
+      if (images) {
+        for (const image of images) {
+          this.IO.observe(image);
+        }
       }
     }
-  }
 
-  common(callback = function() {}, entries) {
-    entries.forEach(function(entry) {
-      if (entry.isIntersecting && entry.target.dataset.lazyload) {
+    common(callback = function() {}, entries) {
+      entries.forEach(function(entry) {
+        if (entry.isIntersecting && entry.target.dataset.lazyload) {
 
-        if (entry.target.classList.contains('simpleLazyJs')) {
+          if (entry.target.classList.contains('simpleLazyJs')) {
 
-          entry.target.style.backgroundImage =
-            'url("' +
-            entry.target.dataset.lazyload.replace(/,/, '"),url("') +
-            '")';
-        } else {
-          entry.target.src = entry.target.dataset.lazyload;
+            entry.target.style.backgroundImage =
+              'url("' +
+              entry.target.dataset.lazyload.replace(/,/, '"),url("') +
+              '")';
+          } else {
+            entry.target.src = entry.target.dataset.lazyload;
+          }
+
+          entry.target.removeAttribute('data-lazyload');
         }
 
-        entry.target.removeAttribute('data-lazyload');
-      }
-
-      if (entry.isIntersecting && !entry.target.dataset.lazyState) {
-        entry.target.dataset.lazyState = '1';
-        callback(entry.target);
-      }
-    });
+        if (entry.isIntersecting && !entry.target.dataset.lazyState) {
+          entry.target.dataset.lazyState = '1';
+          callback(entry.target);
+        }
+      });
+    }
   }
-}
+})(window, document)
